f7d6b387b1a2033c47224c4fabc791a7c52ef554
sujit27 <sujit271290@gmail.com>
2023-05-03 22:42:01 +0530

    code refactored to implement multiple documents

--- main.py
+++ main.py
@@ -15,33 +15,44 @@ from langchain.vectorstores import Chroma
 
 warnings.filterwarnings("ignore")
 
-filePath = 'input_samples/state_of_the_union.txt'
+filePath1 = 'input_samples/Dynamic_density.txt'
+filePath2 = 'input_samples/Bert_Shepard.txt'
 os.environ["OPENAI_API_KEY"] = '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'
 os.environ["TOKENIZERS_PARALLELISM"] = 'false'
 
 def main():
-    print('start')
-    loader = TextLoader(filePath, encoding='utf8')
-    documents = loader.load()
-    print("The documents are loaded")
+    
+    loader1 = TextLoader(filePath1, encoding='utf8')
+    loader2 = TextLoader(filePath2, encoding='utf8')
+
+    ### For multiple documents 
+    loaders = [loader1,loader2]
+    documents = []
+    for loader in loaders:
+        documents.extend(loader.load())
+    # print("The documents are loaded")
     
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
-    print("Text split completed")
+    # print("Text split completed")
 
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    print("Embedding model loaded")
+    # print("Embedding model loaded")
     db = Chroma.from_documents(texts, embeddings)
-    print("Embeddings generated")
+    # print("Embeddings generated")
 
     retriever = db.as_retriever()
 
     qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever)
 
-    query = "What did the president say about children"
-    print("Running the query...")
-    print(qa.run(query))
+    while True:
+        query = input("Enter a query, or press q/Q to exit:\n")
+        if query.lower() == 'q':
+            break
+        else:
+            print("Running the query...")
+            print(qa.run(query))
 
 
 if __name__ == '__main__':
